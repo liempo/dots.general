05f418c9ac76bc148ef1d52c1adc6ed3ca1962c7
Liempo <gonzalesalec@gmail.com>
2020-12-01 14:44:07 +0800

    Create a script that fixes issue with android studio and bspwm

---
+++ .bin/android_studio_bspwm_fix.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+STUDIO_PATH=/opt/android-studio
+JVM_PATH=/usr/lib/jvm/default
+
+if [[ $1 = "revert" ]]; then
+    unlink $STUDIO_PATH/jre
+    mv $STUDIO_PATH/jre_bundled $STUDIO_PATH/jre
+else
+    # Back up bundled jre in studio
+    mv $STUDIO_PATH/jre $STUDIO_PATH/jre_bundled
+    ln -sf $JVM_PATH $STUDIO_PATH/jre
+fi
